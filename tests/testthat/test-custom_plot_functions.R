8ad94477a03b8185fa79df3ef77483f02f954a0d
mndpln <mariefel.deypalan@gmail.com>
2021-06-05 22:27:55 +0800

    Updating test for plot_category

--- tests/testthat/test-custom_plot_functions.R
+++ tests/testthat/test-custom_plot_functions.R
@@ -2,10 +2,10 @@
 library(ggplot2)
 library(vdiffr)
 
-test_that("gg.category produces correct plots", {
+test_that("plot_category produces correct plots", {
   requireNamespace("ggplot2", quietly = T)
-  box_out = gg.category(diamonds, boxplot, clarity, price)
-  hist_out = gg.category(diamonds, histogram, clarity, price)
+  box_out = plot_category(diamonds, boxplot, clarity, price)
+  hist_out = plot_category(diamonds, histogram, clarity, price)
   vdiffr::expect_doppelganger("boxplot from function", box_out)
   vdiffr::expect_doppelganger("histogram from function", hist_out)
 })
